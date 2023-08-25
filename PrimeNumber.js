62592889acc06e6532f7fb48625365cd49452f9b
Maneesha-Thakur <manithakur4253@gmail.com>
2023-08-25 16:38:59 +0530

    Prime Number

---
+++ PrimeNumber.js
@@ -0,0 +1,13 @@
+let num=12
+let flag=true
+for(let i=2;i<num;i){
+    if(num%i==0){
+        flag=false
+        break;
+    }
+}
+if(flag){
+    console.log("Is Prime Number")
+}else{
+    console.log("Not a prime number")
+}
